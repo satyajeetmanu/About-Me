6002786f1f862f5b2b6c7261bd072b7ed8b17c93
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2021-12-08 11:12:51 +0530

    added view binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -8,31 +8,37 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var doneButton: Button
-    lateinit var nicknameEdit: EditText
-    lateinit var nicknameText: TextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        doneButton = findViewById(R.id.done_button)
-        nicknameEdit = findViewById(R.id.nickname_edit)
-        nicknameText = findViewById(R.id.nickname_textview)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        doneButton.setOnClickListener{
+//        doneButton.setOnClickListener{
+//            displayNickname(it)
+//        }
+        
+        binding.doneButton.setOnClickListener{
             displayNickname(it)
         }
+        
     }
 
     private fun displayNickname(view: View){
-        nicknameText.text = nicknameEdit.text
-        doneButton.visibility = View.GONE
-        nicknameEdit.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
+        binding.apply {
+            nicknameTextview.text = binding.nicknameEdit.text
+            invalidateAll()
+            doneButton.visibility = View.GONE
+            nicknameEdit.visibility = View.GONE
+            nicknameTextview.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
